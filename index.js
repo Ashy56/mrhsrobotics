5ff4c4370b13583ae787b2b79b1cbe69ec61e68c
Ashley Biggs <ashleyrose64@gmail.com>
2018-01-24 22:41:18 -0700

    Home button now opens home.html in middleIframe

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 function initialize() {
     document.getElementById("4284").addEventListener("click", team1);
     document.getElementById("7795").addEventListener("click", team2);
-    document.getElementById("FTC").addEventListener("click", FTC);
-    document.getElementById("Gracious").addEventListener("click", Gracious);
+    document.getElementById("FTC").addEventListener("click", ftc);
+    document.getElementById("Gracious").addEventListener("click", gracious);
+    document.getElementById("home").addEventListener("click", home);
 }
 
 function team1() {
-    document.getElementById("middleIframe").src="../4284.html";
+    document.getElementById("middleIframe").src="4284.html";
 
 }
 
 function team2(){
-    document.getElementById("middleIframe").src="../7795.html";
+    document.getElementById("middleIframe").src="7795.html";
 }
 
-function FTC(){
-    document.getElementById("middleIframe").src="../ftc.html";
+function ftc(){
+    document.getElementById("middleIframe").src="ftc.html";
 }
 
-function Gracious(){
-    document.getElementById("middleIframe").src="../gracious.html";
+function gracious(){
+    document.getElementById("middleIframe").src="gracious.html";
+}
+function home(){
+    document.getElementById("middleIframe").src="home.html";
 }
 
